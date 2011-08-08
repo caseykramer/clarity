5ccb4b07ae158c5fc61e2f19a6a481a3dfdb1b7c
Casey.Kramer <casey.kramer@gmail.com>
2011-08-08 19:28:00 -0400

    Added basic support for nested bindings using a simple heuristic that looks for a constructor with the fewest parameters

--- src/main/scala/org/drrandom/Kernel.scala
+++ src/main/scala/org/drrandom/Kernel.scala
@@ -1,13 +1,36 @@
 package org.drrandom
 import collection.mutable.HashMap
 import scala.None
-import reflect._
+import java.lang.Class
+import java.lang.reflect._
+import reflect.{Manifest}
 
 class BindingInfo[T,U](val source:Manifest[T],dest:Manifest[U]) {
 
+  val creator: () => Any = createBuilder
   def erasedType = dest.erasure
-  def create:Any = erasedType.newInstance()
+  def create:Any = creator()
+  var kernel:Kernel = null
+
+  private def createBuilder:() => Any = {
+    val bestCtor = findBestConstructor
+    val params = bestCtor.getParameterTypes
+    if(params.size == 0)
+      return () => bestCtor.newInstance()
+    else
+      return () => {
+        val args = params.map(p => kernel.get(p) match {
+          case Some(i) => i.asInstanceOf[java.lang.Object]
+          case None => throw new BindingException("Unable to find dependent type: "+p.toString+" while creating instance of "+erasedType.toString)
+        }).toArray
+        bestCtor.newInstance(args:_*)
+      }
+  }
+
+  def findBestConstructor = dest.erasure.getConstructors.min(Ordering[Int].on[Constructor[_]](_.getParameterTypes.size))
 }
+
+class BindingException(message:String) extends Exception(message)
 
 class InstanceBindingInfo[T,U](override val source:Manifest[T], dest:Manifest[U],instance:U) extends BindingInfo[T,U](source,dest) {
   override def create:Any = instance
@@ -27,33 +50,35 @@ trait TypeBinder[T] {
 }
 
 abstract class Kernel {
-  def +=(binding:BindingInfo[_,_]):Unit
-  def register(binding:BindingInfo[_,_]):Unit
-  def ++=(binding:Iterable[BindingInfo[_,_]]):Unit
-  def register(binding:Iterable[BindingInfo[_,_]]):Unit
+  def +=(binding:BindingInfo[_,_])
+  def register(binding:BindingInfo[_,_])
+  def ++=(binding:BindingInfo[_,_]*)
+  def register(binding:BindingInfo[_,_]*)
   def get[T](implicit man:Manifest[T]):Option[T]
+  def get(clazz:java.lang.Class[_]):Option[Any]
 }
 
 protected class StandardKernel extends Kernel {
-  class BasicTypeBinder[T](implicit manifest:Manifest[T]) extends TypeBinder[T] {
-    val sourceType = manifest
-  }
-
+  
   private var _bindings = HashMap[Manifest[_],BindingInfo[_,_]]()
 
   def +=(binding:BindingInfo[_,_]) = {
+    binding.kernel = this
     _bindings += binding.source -> binding
   }
 
   def register(binding:BindingInfo[_,_]) = {
+    binding.kernel = this
     _bindings += binding.source -> binding
   }
 
-  def ++=(binding:Iterable[BindingInfo[_,_]]) = {
+  def ++=(binding:BindingInfo[_,_]*) = {
+    binding.foreach(_.kernel = this)
     _bindings ++= binding.map(b => b.source -> b)
   }
 
-  def register(binding:Iterable[BindingInfo[_,_]]) = {
+  def register(binding:BindingInfo[_,_]*) = {
+    binding.foreach(_.kernel = this)
     _bindings ++= binding.map(b => b.source -> b)
   }
 
@@ -61,6 +86,17 @@ protected class StandardKernel extends Kernel {
     _bindings.get(man) match {
       case None => None
       case Some(b:BindingInfo[T,_]) =>Some(b.create.asInstanceOf[T])
+    }
+  }
+
+  def get(clazz:java.lang.Class[_]):Option[Any] = {
+    val keys = _bindings.keys.filter(_.erasure == clazz)
+    if (keys.size == 0)
+      return None
+
+    _bindings.get(keys.head) match {
+      case None => None
+      case Some(b:BindingInfo[_,_]) => Some(b.create)
     }
   }
 }
--- src/test/scala/org/drrandom/tests/StandardKernelTests.scala
+++ src/test/scala/org/drrandom/tests/StandardKernelTests.scala
@@ -1,34 +1,52 @@
 package org.drrandom.tests {
 
-  import org.scalatest.matchers.ShouldMatchers
-  import org.scalatest.Spec
-  import org.drrandom.{Bind, Kernel}
+  import org.specs2.mutable._
+  import org.drrandom._
 
-  class StandardKernelTests extends Spec with ShouldMatchers {
+    class StandardKernelTests extends Specification {
 
-    describe("A kernel") {
-      it("should provide a way to register a type with the container") {
+    "A kernel" should {
+      "provide a way to register a type with the container" in {
         val kernel = Kernel()
-        kernel +=(Bind[ITest].To[TestClass])
+        kernel += Bind[ITest].To[TestClass]
         val found = kernel.get[ITest]
-        found should not be None
+        found must not be None
       }
-      it("should provide a way to register an instance with the container") {
+      "should provide a way to register an instance with the container" in {
         val kernel = Kernel()
         val test = new TestClass()
-        kernel += (Bind[ITest].To(test))
+        kernel += Bind[ITest].To(test)
         val resolvedInstance = kernel.get[ITest]
         resolvedInstance should not be None
 
       }
+      "should allow for netsted constructor depdendencies" in {
+        val kernel = Kernel()
+        kernel ++= (Bind[ITest].To[TestClass],Bind[INestedTest].To[TestNested])
+        
+        var iTest = kernel.get[ITest]
+        iTest must not be None
+
+        var result = kernel.get[INestedTest]
+        result.get.nested must not beNull
+      }
+      
+      "should throw binding exception if dependent types are not registered" in {
+        val kernel = Kernel()
+        kernel += Bind[INestedTest].To[TestNested]
+ 
+        kernel.get[INestedTest] must throwA[BindingException]
+        
+      }
     }
   }
-
 }
 
-  trait ITest {
+  trait ITest {}
 
-  }
-  class TestClass extends ITest {
+  class TestClass extends ITest {}
 
+  trait INestedTest {
+    val nested:ITest
   }
+  case class TestNested(val nested:ITest) extends INestedTest {}
